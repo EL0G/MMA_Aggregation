d71d6b59634b58df0703b3518cf729678621deae
EL0G <31num4n@gmail.com>
2026-08-20 21:19:16 -0400

    normalized data from sources

---
+++ MMA-Aggregation-App/ingestion/processing/normalize.py
@@ -0,0 +1,24 @@
+import datetime as dt
+
+DATE_FORMATS = {
+    "pfl": "%a, %b %d, %Y",
+    "ufc": "%b %d, %Y",
+    "rizin": "%Y年%m月%d日",
+}
+
+def normalize_dates(data, source):
+    current_year = dt.datetime.now().year
+    date_format = DATE_FORMATS[source]
+
+    for event_info in data:
+        date_string = event_info["date"]
+
+        if source == "pfl":
+            date_string = date_string + ", " + str(current_year)
+            date_obj = dt.datetime.strptime(date_string, date_format)
+        else:
+            date_obj = dt.datetime.strptime(date_string, date_format)
+
+        event_info["date"] = date_obj.strftime("%Y-%m-%d")
+    return data
+
